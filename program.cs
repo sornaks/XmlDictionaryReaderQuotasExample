8cc861e73819304e42f1f4cff7dcb4a8af3480e3
sornaks <sornaks@microsoft.com>
2015-04-23 11:15:03 -0700

    Changing Max Depth

--- program.cs
+++ program.cs
@@ -1,13 +1,23 @@
 using System;
-using System.Runtime.Serialization;
-using System.Xml;
-using System.Text;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
 
-namespace XmlDictionaryReaderQuotasExample
+namespace SimpleXMLReader
 {
-	[DataContract(Name = "DummyClass", Namespace = "")]
+    [DataContract(Name = "DummyClass", Namespace = "")]
     public class DummyClass
+    {
+        [DataMember]
+        public int SampleInt { get; set; }
+
+        [DataMember]
+        public SomethingElse SomeClass { get; set; }
+    }
+
+    [DataContract(Name = "SomethingElse", Namespace = "")]
+    public class SomethingElse
     {
         [DataMember]
         public int SampleInt { get; set; }
@@ -16,27 +26,30 @@ namespace XmlDictionaryReaderQuotasExample
         public string SampleString { get; set; }
     }
 
-	public class Program
-	{
-		public static void Main()
-		{
-			var dictionaryQuotas = new XmlDictionaryReaderQuotas();
-			dictionaryQuotas.MaxDepth = 1;
-			var input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                "<DummyClass><SampleInt>10</SampleInt>" +
-                "<SampleString>HelloWorld</SampleString></DummyClass>";
-			var encodedInput = Encoding.UTF8.GetBytes(input);
-			var reader = XmlDictionaryReader.CreateTextReader(
-				new MemoryStream(encodedInput),
-				Encoding.UTF8,
-				dictionaryQuotas,
-				onClose: null);
+    public class Program
+    {
+        public static void Main()
+        {
+            var dictionaryQuotas = new XmlDictionaryReaderQuotas();
+            dictionaryQuotas.MaxDepth = 1;
+            var input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<DummyClass><SampleInt>10</SampleInt><SomeClass><SampleInt>100</SampleInt>" +
+                "<SampleString>HelloWorld</SampleString></SomeClass></DummyClass>";
+            var encodedInput = Encoding.UTF8.GetBytes(input);
+            using (var reader = XmlDictionaryReader.CreateTextReader(
+                new MemoryStream(encodedInput),
+                Encoding.UTF8,
+                dictionaryQuotas,
+                onClose: null))
+            {
+                var serializer = new DataContractSerializer(typeof(DummyClass), new DataContractSerializerSettings());
+                var obj = serializer.ReadObject(reader) as DummyClass;
 
-			var serializer = new DataContractSerializer(typeof(DummyClass));
-			var obj = serializer.ReadObject(reader);
-
-			Console.WriteLine((obj as DummyClass).SampleInt);
-			Console.WriteLine((obj as DummyClass).SampleString);
-		}
-	}
+                Console.WriteLine(dictionaryQuotas.MaxDepth);
+                Console.WriteLine(obj.SampleInt);
+                Console.WriteLine(obj.SomeClass.SampleInt);
+                Console.WriteLine(obj.SomeClass.SampleString);
+            }
+        }
+    }
 }
